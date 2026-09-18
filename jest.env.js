10e108643641a74b13d623127b5e85f7fb6be947
lio311 <lior31197@gmail.com>
2026-09-19 00:54:46 +0300

    Fix ESLint errors in jest.env.js

--- jest.env.js
+++ jest.env.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 const { TextEncoder, TextDecoder } = require('util');
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
