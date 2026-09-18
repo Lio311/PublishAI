808902e83dabc0fb15546392fa10dc826e52c365
lio311 <lior31197@gmail.com>
2026-09-19 00:35:16 +0300

    Fix CI test crash by injecting dummy env vars

--- jest.env.js
+++ jest.env.js
@@ -15,3 +15,7 @@ global.Request = Request;
 global.Response = Response;
 global.Headers = Headers;
 global.fetch = fetch;
+process.env.DATABASE_URL = "postgres://dummy:dummy@dummy/dummy";
+process.env.HF_TOKEN = "dummy_token";
+process.env.MASTER_ENCRYPTION_KEY = "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef";
+process.env.OPENAI_API_KEY = "dummy_sk";
