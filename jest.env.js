c030be191e1c4a232d4ef5bdbbedd0e5c09a32e4
lio311 <lior31197@gmail.com>
2026-09-19 00:50:46 +0300

    Fix jest env undici require for Node 20 CI

--- jest.env.js
+++ jest.env.js
@@ -1,20 +1,31 @@
-const { TextEncoder, TextDecoder } = // eslint-disable-next-line @typescript-eslint/no-require-imports
-require('util');
+const { TextEncoder, TextDecoder } = require('util');
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
 
-const { ReadableStream, WritableStream, TransformStream } = // eslint-disable-next-line @typescript-eslint/no-require-imports
-require('stream/web');
+const { ReadableStream, WritableStream, TransformStream } = require('stream/web');
 global.ReadableStream = ReadableStream;
 global.WritableStream = WritableStream;
 global.TransformStream = TransformStream;
 
-const { Request, Response, Headers, fetch } = // eslint-disable-next-line @typescript-eslint/no-require-imports
-require('undici');
-global.Request = Request;
-global.Response = Response;
-global.Headers = Headers;
-global.fetch = fetch;
+// Node 18+ has native fetch API
+if (typeof fetch !== 'undefined') {
+  global.fetch = fetch;
+  global.Request = Request;
+  global.Response = Response;
+  global.Headers = Headers;
+} else {
+  // Fallback for older environments if needed
+  try {
+    const undici = require('undici');
+    global.fetch = undici.fetch;
+    global.Request = undici.Request;
+    global.Response = undici.Response;
+    global.Headers = undici.Headers;
+  } catch (e) {
+    console.warn('Could not load undici fallback');
+  }
+}
+
 process.env.DATABASE_URL = "postgres://dummy:dummy@dummy/dummy";
 process.env.HF_TOKEN = "dummy_token";
 process.env.MASTER_ENCRYPTION_KEY = "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef";
