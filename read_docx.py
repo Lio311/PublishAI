c80228d8066d74f4d28281aea7c89ddded03fbd2
lio311 <lior31197@gmail.com>
2026-09-19 01:19:47 +0300

    chore: remove junk files from root directory

--- read_docx.py
+++
@@ -1,5 +0,0 @@
-from docx import Document
-doc = Document('PublishAI_Project_Plan_Integrated.docx')
-for i, p in enumerate(doc.paragraphs):
-    if p.text.strip():
-        print(f"{i}: {p.text}")
--- test_inngest.js
+++
@@ -1,2 +0,0 @@
-import { Inngest } from "inngest";
-console.log(Object.keys(Inngest));
