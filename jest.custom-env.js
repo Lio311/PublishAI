8d39af13377ad26a2d6c8eee7306c08831918a35
lio311 <lior31197@gmail.com>
2026-09-19 01:15:24 +0300

    Fix CI again: disable eslint require error in jest.custom-env.js

--- jest.custom-env.js
+++ jest.custom-env.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 const JSDOMEnvironment = require('jest-environment-jsdom').default || require('jest-environment-jsdom');
 
 class CustomEnvironment extends JSDOMEnvironment {
